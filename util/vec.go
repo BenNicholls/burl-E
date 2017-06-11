b0189067103a5181ebd25554e4454018e73e8437
Benjamin Nicholls <nicholls.ben@gmail.com>
2017-06-11 05:40:04 -0400

    added util.Coord, changed Vec2 to floats

--- util/vec.go
+++ util/vec.go
@@ -2,19 +2,59 @@ package util
 
 import "math"
 
-type Vec2 struct {
+//Coord is an (X, Y) pair that represents a spot on some 2d grid. Effectively just an implenetation of util.Vec2 using ints.
+type Coord struct {
 	X, Y int
 }
 
-func (v *Vec2) Set(x, y int) {
+func (c Coord) Get() (int, int) {
+	return c.X, c.Y
+}
+
+func (c *Coord) Move(dx, dy int) {
+	c.X += dx
+	c.Y += dy
+}
+
+func (c *Coord) MoveTo(x, y int) {
+	c.X, c.Y = x, y
+}
+
+func (c1 Coord) Add(c2 Coord) Coord {
+	return Coord{c1.X + c2.X, c1.Y + c2.Y}
+}
+
+func (c1 Coord) Sub(c2 Coord) Coord {
+	return Coord{c1.X - c2.X, c1.Y - c2.Y}
+}
+
+func (c Coord) Mag() int {
+	return int(c.ToVector().Mag())
+}
+
+func (c Coord) ToVector() Vec2 {
+	return Vec2{float64(c.X), float64(c.Y)}
+}
+
+//Vec2 is a 2 dimensional vector
+type Vec2 struct {
+	X, Y float64
+}
+
+func (v *Vec2) Set(x, y float64) {
 	v.X, v.Y = x, y
 }
 
-func (v Vec2) Get() (int, int) {
+func (v Vec2) Get() (float64, float64) {
 	return v.X, v.Y
 }
 
-func (v *Vec2) Mod(dx, dy int) {
+//Returns (X, Y) as ints, rounding as necessary.
+func (v Vec2) GetInt() (int, int) {
+	return int(v.X + math.Copysign(0.5, v.X)), int(v.Y + math.Copysign(0.5, v.Y))
+}
+
+func (v *Vec2) Mod(dx, dy float64) {
 	v.X += dx
 	v.Y += dy
 }
@@ -28,16 +68,12 @@ func (v1 Vec2) Sub(v2 Vec2) Vec2 {
 	return Vec2{v1.X - v2.X, v1.Y - v2.Y}
 }
  
-func (v Vec2) MagFloat() float64 {
-	return math.Sqrt(float64(v.X)*float64(v.X) + float64(v.Y)*float64(v.X))
+func (v Vec2) Mag() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
  }
 
-func (v Vec2) Mag() int {
-	return int(v.MagFloat())
-}
-
 func (v Vec2) ToPolar() Vec2Polar {
-	return Vec2Polar{v.MagFloat(), math.Atan2(float64(v.Y), float64(v.X))}
+	return Vec2Polar{v.Mag(), math.Atan2(v.Y, v.X)}
 }
 
 type Vec2Polar struct {
@@ -52,6 +88,11 @@ func (v Vec2Polar) Get() (float64, float64) {
 	return v.R, v.Phi
 }
 
+//Add converts to recitlinear components and adds, then converts back to polar.
+func (v1 Vec2Polar) Add(v2 Vec2Polar) Vec2Polar {
+	return v1.ToRect().Add(v2.ToRect()).ToPolar()
+}
+
 func (v Vec2Polar) ToRect() Vec2 {
-	return Vec2{int(v.R*math.Cos(v.Phi)), int(v.R*math.Sin(v.Phi))}
+	return Vec2{v.R*math.Cos(v.Phi), v.R*math.Sin(v.Phi)}
 }
