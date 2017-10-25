2c035cedb5cc481cfb378e676cb5b39d84fdb7da
Benjamin Nicholls <nicholls.ben@gmail.com>
2017-10-25 13:43:21 -0400

    preliminary rexpaint art support! only does .csv export files for now, need to write up a parser for the binary format later

--- burl/tileview.go
+++ burl/tileview.go
@@ -1,5 +1,10 @@
 package burl
 
+import "encoding/csv"
+import "os"
+import "strconv"
+import "strings"
+
 //View object for drawing tiles. (eg. maps). Effectively a buffer for drawing before the console grabs it.
 type TileView struct {
 	UIElement
@@ -46,5 +51,47 @@ func (tv TileView) Render(offset ...int) {
 func (tv *TileView) Reset() {
 	for i, _ := range tv.grid {
 		tv.grid[i].Clear()
+	}
+}
+
+//Loads an image exported from RexPaint into CSV format. 
+//TODO: write an actual .xp library for Go! Doesn't seem to be one!
+func (tv *TileView) LoadImageFromCSV(filename string) {
+	if !strings.HasSuffix(filename, ".csv") {
+		LogError("Cannot load image " + filename + "(not csv file!)")
+		return
+	}
+
+	//open image file
+	f, err := os.Open(filename)
+	defer f.Close()
+	if err != nil {
+		LogError("Could not load image data: " + filename)
+		return
+	}
+	
+	//read records/fields from csv into string[][]
+	data, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		LogError("Could not read csv data: " + filename)
+		return
+	}
+
+	//parse records (record 0 is header data)
+	for i := 1; i < len(data); i++ {
+		x, _ := strconv.ParseInt(data[i][0], 10, 0)
+		y, _ := strconv.ParseInt(data[i][1], 10, 0)
+		
+		if int(x) >= tv.width || int(y) >= tv.height {
+			continue
+		}
+
+		glyph, _ := strconv.ParseInt(data[i][2], 10, 0)
+		f, _ := strconv.ParseInt(data[i][3][1:], 16, 0)
+		fore := ChangeAlpha(uint32(f), 0xFF)
+		b, _ := strconv.ParseInt(data[i][4][1:], 16, 0)
+		back := ChangeAlpha(uint32(b), 0xFF)
+
+		tv.grid[int(x)+tv.width*int(y)].SetGlyph(int(glyph), fore, back, tv.z)
 	}
 }
