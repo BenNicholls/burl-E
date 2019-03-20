01552f3f394bff45feedacd4e243fd11a88c3140
Benjamin Nicholls <nicholls.ben@gmail.com>
2019-03-20 01:49:23 -0400

    progressbar code cleanup

--- burl/progressbar.go
+++ burl/progressbar.go
@@ -3,7 +3,7 @@ package burl
 //ProgressBar is a textbox whose background can be a progress bar. Yes.
 type ProgressBar struct {
 	Textbox
-	progresscolour uint32
+	progressColour uint32
 	progress       int //percentage
 	barWidth       int
 }
@@ -14,17 +14,19 @@ func NewProgressBar(w, h, x, y, z int, bord, cent bool, txt string, c uint32) *P
 
 //Takes a percentage value, clamped to 0 <= i <= 100
 func (pb *ProgressBar) SetProgress(i int) {
-	pb.progress = Clamp(i, 0, 100)
-	pb.CalcBarWidth()
+	if pb.progress != Clamp(i, 0, 100) { 
+		pb.progress = Clamp(i, 0, 100)
+		pb.calcBarWidth()
+	}
 }
 
+//Moves the progress bar by a percentage delta d
 func (pb *ProgressBar) ChangeProgress(d int) {
 	pb.SetProgress(pb.progress + d)
-	pb.CalcBarWidth()
 }
 
-func (pb *ProgressBar) CalcBarWidth() {
-	pb.barWidth = int(float32(pb.progress) * float32(pb.width) / float32(100))
+func (pb *ProgressBar) calcBarWidth() {
+	pb.barWidth = int(float64(pb.progress) * float64(pb.width) / 100)
 	if pb.barWidth == 0 && pb.progress != 0 {
 		pb.barWidth = 1
 	} else if pb.progress == 100 {
@@ -37,12 +39,11 @@ func (pb *ProgressBar) GetProgress() int {
 }
 
 func (pb *ProgressBar) SetProgressColour(c uint32) {
-	pb.progresscolour = c
+	pb.progressColour = c
 }
 
 func (pb *ProgressBar) Render() {
 	if pb.visible {
-
 		//need to set bgcolor to COL_NONE so we can draw text over the progressbar without borking it
 		bg := pb.backColour
 		pb.SetBackColour(COL_NONE)
@@ -52,7 +53,7 @@ func (pb *ProgressBar) Render() {
 		for i := 0; i < pb.width; i++ {
 			for j := 0; j < pb.height; j++ {
 				if i < pb.barWidth {
-					console.ChangeBackColour(i+pb.x, j+pb.y, pb.z, pb.progresscolour)
+					console.ChangeBackColour(i+pb.x, j+pb.y, pb.z, pb.progressColour)
 				} else {
 					console.ChangeBackColour(i+pb.x, j+pb.y, pb.z, pb.backColour)
 				}
